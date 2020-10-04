afd8414dae84eab5a4d8df211c9d645cbb6355b1
yjcho <jinjanic91@gmail.com>
2020-10-04 23:55:06 +0900

    home화면완료

---
+++ server/models/Video.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const videoSchema = mongoose.Schema(
+  {
+    writer: {
+      type: Schema.Types.ObjectId, //이렇게 넣으면 User model의 모든 정보를 가져올 수 있음
+      ref: 'User',
+    },
+    title: {
+      type: String,
+      maxlength: 50,
+    },
+    description: {
+      type: String,
+    },
+    privacy: {
+      type: Number,
+    },
+    filePath: {
+      type: String,
+    },
+    category: {
+      type: String,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    duration: {
+      type: String,
+    },
+    thumbnail: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const Video = mongoose.model('Video', videoSchema);
+
+module.exports = { Video };
---
+++ server/routes/video.js
@@ -0,0 +1,112 @@
+const express = require('express');
+const router = express.Router();
+const { Video } = require('../models/Video');
+
+const { auth } = require('../middleware/auth');
+const multer = require('multer');
+var ffmpeg = require('fluent-ffmpeg');
+
+//Storage MULTER CONFIG
+let storage = multer.diskStorage({
+  //파일을 올리면 어디에 그 파일을 저장할지를 정함.
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  //어떠한 파일이름으로 저장을 할 지. (여기서는 날짜 + 원본파일이름)
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}_${file.originalname}`);
+  },
+  //파일 확장자 검색. (비디오 mp4확장자만 업로드 가능)
+  fileFilter: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    if (ext !== '.mp4') {
+      return cb(res.status(400).end('Only JPG, PNG, MP4 is allowed'), false);
+    }
+    cb(null, true);
+  },
+});
+
+const upload = multer({ storage: storage }).single('file');
+
+//=================================
+//             Video
+//=================================
+router.post('/uploadfiles', (req, res) => {
+  //비디오를 서버에 저장한다.
+  upload(req, res, (err) => {
+    if (err) {
+      return res.json({ success: false, err });
+    }
+    //success여부, 파일저장된 곳 주소, 파일 이름
+    return res.json({
+      success: true,
+      url: res.req.file.path,
+      fileName: res.req.file.filename,
+    });
+  });
+});
+
+//비디오 정보들을 DB에 저장한다.
+router.post('/uploadVideo', (req, res) => {
+  const video = new Video(req.body);
+  video.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    res.status(200).json({ success: true });
+  });
+});
+
+//비디오 정보를 DB에서 가져오기
+router.get('/getVideos', (req, res) => {
+  //비디오를 DB에서 가져와서 클라이언트 쪽으로 보낸다.
+  Video.find()
+    .populate('writer') //user의 모든 정보를 가져오기 위함.
+    .exec((err, videos) => {
+      if (err) return res.status(400).send(err);
+
+      res.status(200).json({ success: true, videos });
+    });
+});
+
+router.post('/thumbnail', (req, res) => {
+  //썸네일을 생성하고 비디오 러닝타임 등의 비디오 정보를 가져오기.
+
+  let filePath = '';
+  let fileDuration = '';
+  //비디오 정보 가져오기
+  ffmpeg.ffprobe(req.body.url, function (err, metadata) {
+    console.dir(metadata);
+    console.log(metadata.format.duration);
+    fileDuration = metadata.format.duration;
+  });
+
+  //썸네일 생성
+  ffmpeg(req.body.url)
+    .on('filenames', function (filenames) {
+      console.log('Will Generate' + filenames.join(', '));
+      console.log(filenames);
+
+      filePath = 'uploads/thumbnails/' + filenames[0];
+    })
+    .on('end', function () {
+      console.log('Screenshots taken');
+      return res.json({
+        success: true,
+        url: filePath,
+        fileDuration: fileDuration,
+      });
+    })
+    .on('error', function (err) {
+      console.error(err);
+      return res.json({ success: false, err });
+    })
+    .screenshots({
+      //will take screenshots at 20, 40, 60, 80% of the video
+      count: 3,
+      folder: 'uploads/thumbnails',
+      size: '320x240',
+      //%b: input basename (filename w/o extension)
+      filename: 'thumbnail-%b.png',
+    });
+});
+
+module.exports = router;
